37139afaf41cf69386368efa545778bb0ac6c295
Mar2102 <129897990+Mar2102@users.noreply.github.com>
2023-04-06 00:15:14 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,7 @@ import numpy as np
 chat_id = 682673597 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
+    t=71
+    for i in range(len(x)):
+        x[i]*=(2/(t**2))
     return x.mean() # Ваш ответ
